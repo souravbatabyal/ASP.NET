dc695f1501ec1be085fe326c9b8e1434b925c926
souravbatabyal <souravbatabyal@hotmail.com>
2015-03-16 22:01:27 +0530

    Create add.cs
    The C# code for adding 2 numbers

---
+++ add.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ADD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void add_button_Click(object sender, EventArgs e)
+        {
+            int add = Convert.ToInt32(firstno_textBox.Text) + Convert.ToInt32(secondno_textBox.Text);
+            add_label.Text = "Sum=" + add.ToString();
+        }
+
+        private void exit_button_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
